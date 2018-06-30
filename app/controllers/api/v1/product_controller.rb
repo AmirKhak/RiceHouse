465d6085cfb138b372a8f54aaab7bbbd2214c391
ak8815 <ak8815@ic.ac.uk>
2018-06-30 15:18:20 +0100

    Added avatar url to the api.

--- app/controllers/api/v1/product_controller.rb
+++ app/controllers/api/v1/product_controller.rb
@@ -3,7 +3,12 @@ module Api
     class ProductController < ApplicationController
       def index
         products = Product.all;
-        render json: { status: 'SUCCESS', message: 'Loaded articles', data: products }, status: :ok
+        avatars = Array.new;
+        products.each do |product|
+          avatar_url = product.avatar.url;
+          avatars.push(avatar_url);
+        end
+        render json: { status: 'SUCCESS', message: 'Loaded products', data: products, avatar: avatars }, status: :ok
       end
     end
   end
